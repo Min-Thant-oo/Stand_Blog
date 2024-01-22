b0cb5739c74d494c7a3f5ca23f02ba735e99c525
Min Thant Oo <minthantoo.ardil@gmail.com>
2024-01-23 00:07:19 +0630

    nav-modified

--- resources/views/components/adminlayout/navbar.blade.php
+++ resources/views/components/adminlayout/navbar.blade.php
@@ -2,7 +2,7 @@
     <div class="navbar-brand-wrapper d-flex justify-content-center">
       <div class="navbar-brand-inner-wrapper d-flex justify-content-between align-items-center w-100">  
         <a class="navbar-brand brand-logo" href="/admin/home"><img src="/admin/images/logo.svg" alt="logo"/></a>
-        <a class="navbar-brand brand-logo-mini" href="index.html"><img src="/admin/images/logo-mini.svg" alt="logo"/></a>
+        <a class="navbar-brand brand-logo-mini" href="/admin/home"><img src="/admin/images/logo-mini.svg" alt="logo"/></a>
         <button class="navbar-toggler navbar-toggler align-self-center" type="button" data-toggle="minimize">
           <span class="mdi mdi-sort-variant"></span>
         </button>
